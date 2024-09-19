27c6f44a34f2b7829eb894ea8531d04f308f4670
CodeExcalibar <Alex.raju1010@gmail.com>
2024-09-19 11:31:45 +0100

    DEV-14 Seeded service table using seeding file and fake.js

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,6 +1,8 @@
 import db from '@/lib/db';
 import { PoolClient } from 'pg';
 
+import { faker } from '@faker-js/faker';
+
 async function createServicesTable(client: PoolClient) {
   try {
     await client.query(`
@@ -16,14 +18,38 @@ async function createServicesTable(client: PoolClient) {
   }
 }
 
+async function seedServices(client: PoolClient) {
+  try {
+    const services = Array.from({ length: 10 }).map(() => ({
+      serviceName: faker.person.jobTitle(),
+    }));
+
+    await Promise.all(
+      services.map(({ serviceName }) =>
+        client.query(
+          `INSERT INTO services (service_name)
+                    VALUES ($1)
+                    `,
+          [serviceName]
+        )
+      )
+    );
+
+    console.log('Service table has bee succesfully seeded🌱');
+  } catch (err) {
+    console.error('Error while seeding: ', err);
+    throw err;
+  }
+}
+
 export async function GET() {
   const client = await db.connect();
+
   try {
     await createServicesTable(client);
+    await seedServices(client);
 
     return Response.json({ message: 'Database created successfully' });
-
-    // await seedServices()
   } catch (err) {
     return Response.json({ err });
   } finally {
