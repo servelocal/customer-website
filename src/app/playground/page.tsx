524896e588259234cd86b878f9874fdf43c5d24b
CodeExcalibar <Alex.raju1010@gmail.com>
2024-09-30 23:24:30 +0100

    DEV-18 Setting up scss and dynamic icon buttons

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,15 +1,27 @@
 import Button from '@/components/Button';
+import styles from './page.module.css';
+import { LuHome } from 'react-icons/lu';
+import { MdClose } from 'react-icons/md';
 
 export default function PlaygroundPage() {
   return (
     <main>
       <h1>Play with your components</h1>
-      <div>
+      <div className={styles.buttonsContainer}>
         <Button variant="primary">primary</Button>
         <Button variant="secondary">secondary</Button>
         <Button variant="success">Success</Button>
         <Button variant="danger">Danger</Button>
         <Button variant="warning">Warning</Button>
+
+        <Button variant="primary" icon={<LuHome />} iconPosition="left">
+          Icon Left
+        </Button>
+        <Button variant="primary" icon={<LuHome />} iconPosition="right">
+          Icon Right
+        </Button>
+
+        <Button variant="primary" icon={<MdClose />} ariaLabel="close"></Button>
       </div>
     </main>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,12 @@
-import styles from './Button.module.css';
+import styles from './Button.module.scss';
+import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: keyof typeof variants;
   className?: string;
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
+  ariaLabel?: string; // Optional aria-label for better accessibility
 }
 
 const variants = {
@@ -16,9 +20,43 @@ const variants = {
 export default function Button({
   variant = 'primary',
   className = '',
+  icon,
+  iconPosition = 'left',
+  ariaLabel,
+  children,
   ...props
 }: ButtonProps) {
   const buttonClass = `${styles.button} ${variants[variant]} ${className}`;
 
-  return <button {...props} className={buttonClass}></button>;
+  const renderContent = () => {
+    const hasChildren = !!children;
+
+    return (
+      <>
+        {icon && iconPosition === 'left' && (
+          <span
+            className={`${styles.icon} ${hasChildren ? styles.iconLeft : ''}`}
+            aria-hidden="true"
+          >
+            {icon}
+          </span>
+        )}
+        {children}
+        {icon && iconPosition === 'right' && (
+          <span
+            className={`${styles.icon} ${hasChildren ? styles.iconRight : ''}`}
+            aria-hidden="true"
+          >
+            {icon}
+          </span>
+        )}
+      </>
+    );
+  };
+
+  return (
+    <button {...props} className={buttonClass} aria-label={ariaLabel}>
+      {renderContent()}
+    </button>
+  );
 }
