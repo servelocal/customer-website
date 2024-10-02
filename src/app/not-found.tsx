10344954b891df5c95b6df7dd90107e1bbc2a92f
CodeExcalibar <Alex.raju1010@gmail.com>
2024-10-02 09:04:35 +0100

    DEV-19 Changed from css to sass

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import styles from './notFound.module.css';
+import styles from './notFound.module.sass';
 import { usePathname } from 'next/navigation';
 import Button from '@/components/Button';
 
