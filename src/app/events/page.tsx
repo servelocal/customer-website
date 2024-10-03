ebdbeb31847cf652835fe98b59f912903ed6fb34
amltms <amalthomas587@gmail.com>
2024-10-03 20:45:22 +0100

    create events page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Events() {
+  return (
+    <main>
+      <h1>This is the event page</h1>
+      <Link href="/playground">Go to playground 🤜</Link>
+    </main>
+  );
+}
