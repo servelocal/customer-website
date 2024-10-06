d4d89bf91973f2f36840c34433be8df10b8acda2
CodeExcalibar <Alex.raju1010@gmail.com>
2024-10-06 21:16:01 +0100

    DEV 25 Navbar support responsiveness, improved code, componentized navlinks

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,47 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import styles from './Navbar.module.scss';
+import NavLinks from './NavLinks';
+import { LuMenu } from 'react-icons/lu';
+import Button from '../Button';
+
+const itemLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'Event', href: '/events' },
+  { name: 'Playground', href: '/playground' },
+];
 
 const Navbar = () => {
-  const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Link href="/">ServeLocal</Link>
+      <div>
+        <Link className={styles.text} href="/">
+          ServeLocal
+        </Link>
       </div>
-      <ul className={styles.navLinks}>
-        <li>
-          <Link
-            href="/events"
-            className={pathname === '/events' ? styles.active : ''}
-          >
-            Events
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/playground"
-            className={pathname === '/playground' ? styles.active : ''}
-          >
-            Playground
-          </Link>
-        </li>
+
+      <Button
+        ariaLabel="Navbar toggle"
+        className={styles.menuToggle}
+        onClick={toggleMenu}
+      >
+        <span className={styles.icon}>
+          <LuMenu />
+        </span>
+      </Button>
+
+      <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ''}`}>
+        {itemLinks.map(({ name, href }) => (
+          <NavLinks key={name} name={name} href={href} />
+        ))}
       </ul>
     </nav>
   );
