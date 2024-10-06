abfedd78e8a0516b9fba70fa557d085c0a83a833
CodeExcalibar <Alex.raju1010@gmail.com>
2024-10-06 23:18:26 +0100

    Installed prisma client, working db query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 import styles from './page.module.scss';
 import { prisma } from '@/lib/prisma';
 
+interface Event {
+  event_id: number; // Use 'number' instead of 'Integer'
+  name: string; // Use 'string' instead of 'String'
+}
 export default async function Home() {
-  const events = await prisma.events.findMany();
+  const events: Event[] = await prisma.events.findMany();
 
   return (
     <div className={styles.page}>
