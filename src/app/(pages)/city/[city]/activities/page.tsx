e92bd4fc7442681d26d6865953f8cdd9ce6b94c9
CodeExcalibar <Alex.raju1010@gmail.com>
2024-11-16 12:40:35 +0000

    Fixed params errors

--- src/app/(pages)/city/[city]/activities/page.tsx
+++ src/app/(pages)/city/[city]/activities/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 import Banner from '@/components/Banner';
 import CategorySection from '@/components/CategorySection';
+import { capitalise } from '@/utils/capitalise';
 
 interface Activity {
   activity_id: number;
@@ -10,15 +12,20 @@ interface Activity {
   details: { bannerImage: string; thumbnailImage: string; priceRange: string };
 }
 
-const ActivitiesPage = ({ params }: { params: { city: string } }) => {
+const ActivitiesPage = () => {
   const [activities, setActivities] = useState<Record<string, Activity[]>>({});
-
+  const params: { city?: string | undefined } = useParams(); // Use the useParams hook
+  console.log(params.city);
   useEffect(() => {
     const fetchActivities = async () => {
       const response = await fetch('/api/activities');
       const data = await response.json();
+      console.log(data.activities);
+      console.log(data.city.toLowerCase());
+      console.log(params.city);
 
-      const cityData = data.city.toLowerCase() === params.city.toLowerCase() ? data.activities : [];
+      const cityData = data.city.toLowerCase() === params.city ? data.activities : [];
+      console.log(cityData);
       const categorized = cityData.reduce((acc: Record<string, Activity[]>, activity: Activity) => {
         acc[activity.category] = acc[activity.category] || [];
         acc[activity.category].push(activity);
@@ -32,7 +39,7 @@ const ActivitiesPage = ({ params }: { params: { city: string } }) => {
   return (
     <>
       <Banner
-        title={`Activities in ${params.city.charAt(0).toUpperCase() + params.city.slice(1)}`}
+        title={`Activities in ${capitalise(params.city)}`}
         subtitle="Discover amazing services and activities in your area"
         backgroundImage="https://img.freepik.com/premium-vector/yellow-background-with-dynamic-abstract-shapes_580167-286.jpg"
       />
