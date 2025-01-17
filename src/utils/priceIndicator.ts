71ca6fc393f6676dd5b6cda9641d3700007faee1
amltms <amalthomas587@gmail.com>
2025-01-17 21:31:52 +0000

    Enhance price indicator utility to display 'Free' for zero amount and categorize amounts into tiers

--- src/utils/priceIndicator.ts
+++ src/utils/priceIndicator.ts
@@ -4,6 +4,17 @@ export const createPriceIndicator = (prices: Price[]): string => {
   const priceToUse =
     prices.length === 1 ? prices[0] : prices.find((price) => price.type === 'General') || prices[0];
 
-  const priceIndicator = '£'.repeat(Math.floor(priceToUse.amount / 10));
+  let priceIndicator = '';
+
+  if (priceToUse.amount === 0) {
+    priceIndicator = 'Free';
+  } else if (priceToUse.amount <= 10) {
+    priceIndicator = '£';
+  } else if (priceToUse.amount <= 20) {
+    priceIndicator = '££';
+  } else {
+    priceIndicator = '£££';
+  }
+
   return priceIndicator;
 };
