399288847e198cd1d6f5b583b00af3931c5665d3
amltms <amalthomas587@gmail.com>
2025-01-18 12:28:28 +0000

    Update CategorySection to allow tags to overflow horizontally on hover

--- src/components/CategorySection/CategorySection.tsx
+++ src/components/CategorySection/CategorySection.tsx
@@ -84,7 +84,7 @@ const CategorySection = ({ title, tags, activities }: CategorySectionProps) => {
           </h2>
 
           {/* Sliding Tags (visible when hovering over any part of the title) */}
-          <div className="absolute left-full top-1/2 ml-2 flex translate-x-[-10px] translate-y-[-50%] flex-nowrap gap-2 opacity-0 transition-all duration-500 group-hover/title:translate-x-0 group-hover/title:opacity-100">
+          <div className="absolute left-full top-1/2 ml-2 flex w-screen translate-x-[-10px] translate-y-[-50%] flex-nowrap gap-2 overflow-x-auto opacity-0 transition-all duration-500 group-hover/title:translate-x-0 group-hover/title:opacity-100">
             {tags.map((tag) => (
               <span
                 key={tag}
