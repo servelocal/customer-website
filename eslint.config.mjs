25417ce01b215ef4a497a1c8607282d20489cf0a
amltms <amalthomas587@gmail.com>
2025-01-31 11:10:05 +0000

    refactor: update ESLint configuration to use FlatConfig and improve React rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,22 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').FlatConfig[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Not needed for React 17+
+    },
+  },
+  ...js.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...react.configs.recommended, // Apply recommended React rules
 ];
